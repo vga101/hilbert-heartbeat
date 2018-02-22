22164fd2e5346cec4f624af46bce9151ba4e878e
Oleksandr Motsak <malex984+gh.pub@gmail.com>
2018-02-22 01:54:57 +0100

    Improved HB server
    1. better (secure) parsing of request path to get prefix (command) and all parameters (timeout, appid)
    2. removed Timer usage in favor of iterating global dictionary with all clients before handling any request
    
    Also some cleanup, commenting and minor improvements

--- server/heartbeat.py
+++ server/heartbeat.py
@@ -1,126 +1,162 @@
 #! /usr/bin/env python3
 
-# import sched, time
-from time import time
-from time import sleep
-from random import randint
-from threading import Timer
-
-import os  # environment vars
-
-# import urllib.request, urllib.parse, urllib.error  # what? why?
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import unquote
 
-# https://docs.python.org/2/library/sched.html
-#### SH = sched.scheduler(time, sleep)
+from time import time
+import os  # environment vars
 
 PORT_NUMBER = int(os.getenv('HB_PORT', 8888))
 HOST_NAME = os.getenv('HB_HOST', '127.0.0.1')
 HB_SERVER_URL = os.getenv('HB_URL', "http://" + HOST_NAME + ":" + str(PORT_NUMBER))
 
+visits = {} ### { ID => (StartTime, Timeout, ?TimerObject?, OverdueCounter) }
 
-visits = {}
+# TODO: add better logging?
 
-# overdue = 0 # Just for now... TODO: FIXME?: should be a part of visit data, right?!
+# def toolate(ID):
+#     ts = time()
+#     print("[", ts, "] [", ID, "]: ", visits[ID])
+#
+#     d = visits[ID]
+#     # Collect statistics about ID
+#
+#     if d[3] > 6:
+#         print("TOO overdue - dropping!!!")  # TODO: early detection of overdue clients!!???
+#         del visits[ID]
+#     else:
+#         visits[ID] = (d[0], d[1], None, d[3] + 1) # Timer(d[1], toolate, [ID])
+# #        visits[ID][2].start()  # Another Chance???
 
-# TODO: add logging, fix for Python2 and Python3? Better Timer handling!
-
-def toolate(ID):
+def clear_overdue_visits():
+    global visits
+    # NOTE: no need to lock the global dictionary `visits` since HB server runs synchronously!
+    tmp = {}
     ts = time()
-    print("[", ts, "] [", ID, "]: ", visits[ID])
+    while visits:
+        kv = visits.popitem()
+        ID = kv[0]
+        assert ID not in visits
 
-    d = visits[ID]
-    # Collect statistics about ID    
-
-    if d[3] > 6:
-        print("TOO overdue - dropping!!!")  # TODO: early detection of overdue clients!!???
-        del visits[ID]
-    else:
-        visits[ID] = (d[0], d[1], Timer(d[1], toolate, [ID]), d[3] + 1)
-        visits[ID][2].start()  # Another Chance???
+        d  = kv[1]
+        delta = float(ts - d[0])/float(d[1])
+        if delta > 6.0:
+            print("TOO overdue - dropping: ")  # TODO: early detection of overdue clients!!???
+            print("[", ts, "] [", ID, "]: ", d)
+#            del visits[ID]
+        else:
+            tmp[ID] = (d[0], d[1], d[2], int(delta)) # Timer(d[1], toolate, [ID])
+#            visits[ID][2].start()  # Another Chance???
+    assert not visits
+    visits = tmp
 
 
 class MyHandler(BaseHTTPRequestHandler):
-    def status(s, ID):
+    def status(s, ID=None):
         """
-        s: .headers, .client_address, .command, .path, .request_version, .raw_requestline
+        # query the status of currently tracked visits (compatible with OMD)
         """
-        d = visits[ID]
+        if (ID is None) and (len(visits) == 1):
+            ID = next(iter(visits.keys()))
 
-        t = d[0]
-        dd = d[1]
-        od = d[3]
+        if ID is not None:
+            if ID in visits:
+                # s.status(ID)
+                d = visits[ID]
 
-        app = "application [" + str(ID) + "]| od=" + str(od) + ";1;3;0;10 delay=" + str(dd) + ";;; "
+                t = d[0] # start time
+                dd = d[1] # timeout
+                od = d[3] # current statistics
 
-        ## STATUS CODE???
-        if od <=2:
-            s.wfile.write(bytes("OK - fine " + app, 'UTF-8'))
-        elif od > 2:
-            s.wfile.write(bytes("WARNING - somewhat late " + app, 'UTF-8'))
-        elif od > 4:
-            s.wfile.write(bytes("CRITICAL - quite late " + app, 'UTF-8'))
+                assert (od >= 0)
+
+                # see http://nagios-plugins.org/doc/guidelines.html
+                app = "application [" + str(ID) + "]| od=" + str(od) + ";4;6;0;7 delay=" + str(dd) + ";;; "
+
+                ## STATUS CODE???
+                if od <= 3:
+                    s.wfile.write(bytes("OK - fine " + app, 'UTF-8'))
+                elif od <= 5:
+                    s.wfile.write(bytes("WARNING - somewhat late " + app, 'UTF-8'))
+                else:
+                    s.wfile.write(bytes("CRITICAL - quite late " + app, 'UTF-8'))
+
+            else:
+                s.wfile.write(bytes("CRITICAL - no application record for queried ID: [" + str(ID) + ']', 'UTF-8'))
+        else:
+            if len(visits) > 1:
+                s.wfile.write(bytes(
+                    "WARNING - multiple (" + str(len(visits)) + ") applications: [{}]".format(','.join(visits.keys())),
+                    'UTF-8'))
+            elif len(visits) == 1:
+                s.wfile.write(bytes("UNKNOWN - no heartbeat clients at the moment...", 'UTF-8'))
+
 
     def do_GET(s):
         global visits
         #        global overdue
+        clear_overdue_visits()
 
         s.send_response(200)
         s.send_header('Content-type', 'text/html')
         s.send_header('Access-Control-Allow-Origin', '*')
         s.end_headers()
 
-        # NOTE: URL "localhost:8888/hb_init?48&appid=test_client_python" should be parsed as follows:
-        #         '/hb_init', '48', 'appid=test_client_python']
+        # NOTE: URL "localhost:8888/hb_init?48.2&appid=test_client_python" should be parsed as follows:
+        #         '/hb_init', '48.2', 'appid=test_client_python']
         ### TODO: FIXME: the following url parsing is neither failsafe nor secure! :-(
-        path, _, tail = s.path.partition('?')
-        path = unquote(path)
 
-        if path == "/list":
+        # NOTE: PARSING: s.path as follows: path ? T & appid = ID [& ...] :
+        path = unquote(s.path)
+
+        if path.startswith("/list"):
             # NOTE: list currently tracked visits
             for k, v in visits.items():
                 s.wfile.write(bytes(str(k) + "\n", 'UTF-8'))
             return
 
+        tail = ''
+        if path.find('?') >= 0:
+            path, tail = path.split('?', maxsplit=1)
+
+        assert not (tail.find('?') >= 0)
+
         query = tail.split('&')
 
-        if path == "/status":
-            # NOTE: query the status of currently tracked visits (compatible with )
-            if tail != "":
-                ID = query[0].split('=')[1]  # + " @ " + s.client_address[0] # " || " + s.headers['User-Agent']
-                if ID in visits:
-                    s.status(ID)
-                else:
-                    s.wfile.write(bytes("CRITICAL - no application record for " + str(ID), 'UTF-8'))
-            else:
-                if len(visits) == 1:
-                    ID = next(iter(visits.keys()))
-                    s.status(ID)
-                elif len(visits) > 1:
-                    s.wfile.write(bytes("WARNING - multiple (" + str(len(visits)) + ") applications", 'UTF-8'))
-                else:
-                    s.wfile.write(bytes("UNKNOWN - no heartbeat clients at the moment...", 'UTF-8'))
+        ID = None
+        if (tail != "") and (len(query) > 0):
+            for q in query:
+                if q.startswith('appid='):
+                    ID = q[6:]
+                    break
+        # ID = query[1].split('=')[1]  # + " @ " + s.client_address[0] # " || " + s.headers['User-Agent']
+        # ID = query[0].split('=')[1]  # + " @ " + s.client_address[0] # " || " + s.headers['User-Agent']
 
+#        print("Input request: [{}] -> cmd: [{}], query: {}, ID: [{}]".format(s.path, path, query, ID))
+
+        if path.startswith("/status"):
+            MyHandler.status(s, ID)
             return
 
-        # PARSING: s.path -->>> path ? T & appid = ID [& ...]
-        ID = query[1].split('=')[1] + " @ " + s.client_address[0]  # " || " + s.headers['User-Agent']
-        T = float(query[0])/1000.0 # interval: floating number (in ms = 1/1000 s). Has to be converted into seconds
+#        if ID in visits:
+#            print("PREVIOUS STATE", visits[ID])
+#            visits[ID][2].cancel()  # !
 
-        if ID in visits:
-            print("PREVIOUS STATE", visits[ID])
-            visits[ID][2].cancel()  # !
+        if len(query[0]) == 0:
+            T = 0.0
+        else:  # interval: floating number (in ms = 1/1000 s). Has to be converted into seconds
+            T = float(query[0]) / 1000.0
 
-        ts = time()
+        T = T + 1.0  # (T*17)/16 ? (T*11)/8) ?
+        assert (T > 0.0)
 
+        ts = time() # current time
         if (((path == "/hb_init") or (path == "/hb_ping")) and (ID not in visits)):
             # Hello little brother! Big Brother is watching you!
             print("Creation from scratch : ", ID, " at ", ts)
-            T = T + 1  # max(10, (T*17)/16)
-            visits[ID] = (ts, T, Timer(T, toolate, [ID]), 0)
+            visits[ID] = (ts, T, None, 0) # Timer(T, toolate, [ID])
             s.wfile.write(bytes(str(1000.0*T), 'UTF-8'))  # ?
-            visits[ID][2].start()
+#            visits[ID][2].start()
 
         elif ((path == "/hb_done") and (ID in visits)):
             print("Destruction: ", ID, " at ", ts)
@@ -129,10 +165,11 @@ class MyHandler(BaseHTTPRequestHandler):
 
         elif (((path == "/hb_ping") or (path == "/hb_init")) and (ID in visits)):  #
             # TODO: make sure visits[ID] exists!
-            print("HEART-BEAT for: ", ID, " at ", ts)  # Here i come again... 
+            print("HEART-BEAT for: ", ID, " at ", ts)  # Here i come again...
             lastts = visits[ID][0]
             lastt = visits[ID][1]
             overdue = visits[ID][3]
+
             #            if (ts - lastts) > lastt: # Sorry Sir, but you are too late :-(
             #                overdue += 1
 
@@ -140,10 +177,9 @@ class MyHandler(BaseHTTPRequestHandler):
                 print("Overdue counter: ", overdue)  # TODO: early detection of overdue clients!!???
                 # s.wfile.write("dead")  # ?
             #                del visits[ID] #??
-            T = T + 1  # max(3, (T*11)/8)
-            visits[ID] = (ts, T, Timer(T, toolate, [ID]), 0)
+            visits[ID] = (ts, T, None, 0)  # NOTE: forget previous overdue! # Timer(T, toolate, [ID])
             s.wfile.write(bytes(str(1000.0*T), 'UTF-8'))
-            visits[ID][2].start()
+#            visits[ID][2].start()
             # WHAT ELSE????
         return
 
