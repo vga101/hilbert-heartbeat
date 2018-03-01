7b4b07bf3b2aef9f69b411f4aea12b3395a9be6d
Oleksandr Motsak <malex984@users.noreply.github.com>
2018-03-01 15:30:44 +0100

    Turn-off the usage of the default main-entry from `heartbeat.sh` by `test.sh`

--- client/bash/test.sh
+++ client/bash/test.sh
@@ -4,13 +4,16 @@ export APP_ID='test_bash'
 #HB_PORT='8888'
 export HB_HOST='localhost'
 
-function hb_test() {  # NOTE: should be the same as "$0.sh"!
+# disable the default main-entry of ./heartbeat.sh
+export HB_BASH_LIBRARY=1
+source ./heartbeat.sh
+
+function hb_test() {
   echo "HB_URL: [${HB_URL}]"
   _hb_init 500
   _hb_ping 1000
   _hb_done 2
 }
 
-source ./heartbeat.sh
-
-
+hb_test "$@"
+exit $?
